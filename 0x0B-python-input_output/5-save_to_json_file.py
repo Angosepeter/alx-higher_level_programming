982adb1f70ee5bd6c189ce9e9c49ee690537f4d4
Angosepeter <kevinangose619@gmail.com>
2023-07-11 17:19:30 -0700

    changes

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 """Defines a JSON file-writing function."""
-
 import json
-
 def to_json_string(my_obj):
-    """Write an object to a text file using JSON representation."""
     return json.dumps(my_obj)
