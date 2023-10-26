0963be952bec7be7be436cb99ec08d7eb7d00c45
Angosepeter <kevinangose619@gmail.com>
2023-10-25 22:34:10 -0700

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
--- List top 3 cities
--- Execute: cat 102-top_city.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+-- List avergae temperatures
+-- Execute: cat 101-avg_temperatures.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
